c7e0090795554e22bbe619fd2d320ab28260b7de
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-11 19:10:22 -0400

    Matching key length to text
    ASCII conversion done, matching length of key to plain

--- SRN03.11.m
+++ SRN03.11.m
@@ -4,29 +4,26 @@ function cipher = Vigenere1(plain,key)
 plain = lower(plain);
 key = lower(key);
 
-% Convert lowercase to corresponding numbers
+% Convert lowercase to corresponding numbers (ASCII)
+numbers_plain = double(plain) - 97;
+numbers_key = double(key) - 97; % -97 allows us to begin at 0
 
-length_plain = length(plain_m); %how to get length of string????
-length_key = length(key_m);
-cipher_m = zeros(length_plain);
+%cipher_m = zeros(length_plain);
 
-%%%BOTH INPUTS BECOME NUM MATRICES
-%both plain &  key need to become vectors of same length
-1) separate characters into matrix
-2) characters -> numbers
-%& corresponding numbers (a = 0, z = 25)
-%'lemon' -> [11 4 12 14 13], key [13 2 3 13 2]
-
-
-%%MAKE KEY SAME LENGTH AS PLAINTEXT
-if length_key < length_plain
-    %repeat enough to fit same length
+% MAKE KEY SAME LENGTH AS PLAINTEXT
+repeat_key = numbers_key;
+while length(numbers_key) < length(numbers_plain)
+    repeat_key = [repeat_key repeat_key]
 end
 
+% TRIM repeated key to length of plain
+repeat_key = repeat_key(1:length(numbers_plain));
 
-for ii = 1:length(plain)
+
+end
+%for ii = 1:length(plain)
 
 %if statement needed here?
-modulo((plain_text(ii) + key(ii))26) = cipher(ii);
+%modulo((plain_text(ii) + key(ii))26) = cipher(ii);
 % [24 6 14 27 15]
 % end
