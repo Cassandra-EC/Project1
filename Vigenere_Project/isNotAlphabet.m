2bbb708eb8e6e8d788a320393986120616c19d48
Cassandra-EC <162471376+Cassandra-EC@users.noreply.github.com>
2024-03-11 13:18:27 -0400

    Create isNotAlphabet function
    Determines whether inputs are purely alphabetical (isFalse if any spaces or other non-alphabetical characters are found in input)
    
    Should be done for now. Can change to fit desired results, if we later want to detect (but allow) spaces, punctuation, etc.

---
+++ Vigenere_Project/isNotAlphabet.m
@@ -0,0 +1,16 @@
+%%TO DETERMINE IF INPUTS ARE ALPHABETICAL 
+%No inputs with saces/punctuation; all must be one word, otherwise invalid
+%created 3/11/24 Cassandra
+
+function isFalse = isNotAlphabet(inputString)
+    %put both plain & key (inputString) in lowercase
+    inputString = lower(inputString);
+
+    %match only alphabetical characters, from start to end
+    regexPattern = '^[a-z]+$';
+    
+    %isFalse if any characters are not alphabetical
+    isFalse = isempty(regexp(inputString, regexPattern, 'once')); %any non-matches returns ~true [i.e. false]
+
+end
+
