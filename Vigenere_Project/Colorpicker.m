0f41de212d1a69ce2c487326d98720c325104e0e
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-13 11:40:50 -0400

    Now outputs RGB and HEX key
    Generates both an RGB and hex key (upon user selection using color picker.)

--- Vigenere_Project/Colorpicker.m
+++ Vigenere_Project/Colorpicker.m
@@ -1,14 +1,29 @@
-% TO ALLOW USERS TO PICK COLOR
-% Assigns RGB_key to a 3 value triplet
+% ALLOW USERS TO PICK COLOR
+% Outputs both RGB and Hex keys to be used for encryption
 
 function RGB_key = colorpick()
 
-c = uisetcolor('Choose a color to encrypt with!');
+    c = uisetcolor('Choose a color to encrypt with!');
 
-red_channel = c(1)*255;
-green_channel = c(2)*255;
-blue_channel = c(3)*255;
+    red_channel = c(1)*255;
+    green_channel = c(2)*255;
+    blue_channel = c(3)*255;
 
-RGB_key = [red_channel, green_channel, blue_channel];
+    RGB_key = [red_channel, green_channel, blue_channel];
+    hex_key = rgb2hex(RGB_key);
 
+
+%%% Convert RGB to Hex code
+function hex_key = rgb2hex(c)
+    hex_key = sprintf('#%02X%02X%02X', c(1), c(2), c(3)); %format specifications
+    %# first, then format each r g or b as 2 digits (leading 0's if needed,
+    % %X indicates hex (base 16) conversion
+
+
+disp('RGB key');
+disp(RGB_key);
+disp('Hex key');
+disp(hex_key);
+
+end
 end
