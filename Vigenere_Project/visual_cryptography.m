ed52a156be76b59176812cc38994271140c80163
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-13 21:24:02 -0400

    Notes and such
    Pair programming and discussion w CEC for 2.5 hours

---
+++ Vigenere_Project/visual_cryptography.m
@@ -0,0 +1,17 @@
+%%% VISUAL CRYPTOGRAPHY
+%%% takes in an image and a keyword, outputs encryped image and key word
+% preoutputs: 
+% image made from keyword
+%%%take in image and break apart the channels!!!!!
+
+%word becomes a one channel image, applied the same way to each r g b
+%channel of the original image
+
+%1 take in image and make it greyscale (begin w making it work)
+
+    % find size of image, assign that to a matrix of zeros
+    %turn the word into an image
+        %%%use ASCII and go greyscale first, convert
+        %%% don't care about rows
+
+     
