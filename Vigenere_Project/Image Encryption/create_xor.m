19b9001b20166804c6b764b951e5e322124d9cf6
Cassandra-EC <162471376+Cassandra-EC@users.noreply.github.com>
2024-03-15 19:06:44 -0400

    Further key_to_img1 editing
    There may be a bug [need to confirm] but for now it seems to be working pretty well. Need to check reshape is working

--- Vigenere_Project/Image Encryption/create_xor.m
+++ Vigenere_Project/Image Encryption/create_xor.m
@@ -1,1 +1,12 @@
-function xor_img = create_xor(og_img, key_img)
+function xor_img = create_xor(og_img, key_to_img1)
+%%% The 'XOR' is the difference between each value of the og_img and the
+%%% key_img
+
+%%% USE KEY_TO_IMG1 TO CREATE XOR
+
+
+
+
+
+
+end
--- Vigenere_Project/Image Encryption/key_to_img1.m
+++ Vigenere_Project/Image Encryption/key_to_img1.m
@@ -2,35 +2,35 @@ function simple_key_img = key_to_img1(key, og_img)
 %%%CREATE AN IMAGE OF KEY, THAT IS THE SIZE OF OG_IMG
 % Done 'simply'; iterate word throughout each row
 
-%%% Inputs:
-% key_new = key, repeated to match length of og_img
-% og_img = original img input by user
+    %%% Inputs:
+    % key_new = key, repeated to match length of og_img
+    % og_img = original img input by user
 
-%%%outputs: image created from key
+    %%%outputs: image created from key
 
 
-%=== ACQUIRE SIZE OF KEY & OG_IMG
-% (later will make image from key matching og)
+%=== ACQUIRE CURRENT SIZE OF KEY
 length_k = length(key);
-[m, n] = size(og_img);  % dimensions of og_img);
 
 % numeric (double) version of key, accounting for desired ASCII start
 numeric_k = double(key) - 32;
 
 
-%=== MAKE 
-%if key is larger than image, irrelevant; move on
+%=== MAKE KEY (NUMERIC) MATCH DIMENSIONS OF ORIGINAL IMAGE
+%if key is larger than image, confirm if user wants these inputs
 if length_k > numel(og_img)
     disp(strcat('Are you sure you want', {' '}, key, ' as your key?'));
     % some if yes do x, if no do y
 
     key_new = numeric_k;
+    return;
     %return; %IF WE TRIM DO HERE
     %%%%instead of trim: error message, confirm user wanted that order
 
 % if key = size of img, move on
 elseif length_k == numel(og_img)
     key_new = numeric_k;
+    key_new = reshape(key_new, m, n);
 
 
 % if size of og_img > length_k, repeat numeric_k to match
@@ -40,10 +40,15 @@ else
     
     % key_new = key, repeating until it's the size of img (call
     % remaining indices)
-    key_new = numeric_k(mod(vector_og, length_k) + 1);
-    reshape(key_new, m, n);
+    key_new = numeric_k(mod(og_img, length_k) + 1);
+    key_new = reshape(key_new, size(og_img)); %%need to confirm this works
 
 end
+
+
+% MAKE KEY_NEW FUNCTIONAL AS IMAGE (values 0-255)
+key_new = mod(key_new, 256);
+sized = size(key_new)
 
 
 end
