be5ece8953af2167904e7f3127a10ce0a2743ba6
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-15 02:19:42 -0400

    Complex img-- generating key
    Key will be the numeric key, except the value at each index is duplicated (sequentially) and will then be repeated

--- Vigenere_Project/Image Encryption/key_to_img2.m
+++ Vigenere_Project/Image Encryption/key_to_img2.m
@@ -5,6 +5,19 @@ function complex_key_img = key_to_img2(key, og_img)
 
 % xor_img + complex_key_img = encrypted_img 
 
+[m, n] = size(og_img);
 
+% Initialize variables
+top = 1;
+bottom = m;
+left = 1;
+right = n;
+index = 1;
 
-end
+% Reshape numeric key to have sequential double values for each
+% character -- ie, 1 2 3 becomes 1 1 2 2 3 3
+key_for_spiral = [numeric_k; numeric_k];
+key_for_spiral = key_for_spiral(:)';
+
+%end
+
