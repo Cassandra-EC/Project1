1d63ac0f1a4fe88c1782353f3702a8be6459aaa3
Cassandra-EC <162471376+Cassandra-EC@users.noreply.github.com>
2024-03-15 20:43:21 -0400

    Create_xor completion
    Edits to create_xor and key_to_img1. Both appear to be pretty complete (still just grayscale/1 channel input)

--- Vigenere_Project/Image Encryption/create_xor.m
+++ Vigenere_Project/Image Encryption/create_xor.m
@@ -1,12 +1,18 @@
-function xor_img = create_xor(og_img, key_to_img1)
+function xor_img = create_xor(og_img, key_new1)
 %%% The 'XOR' is the difference between each value of the og_img and the
-%%% key_img
+%%% key_img. Each pixel is a subtraction.
 
-%%% USE KEY_TO_IMG1 TO CREATE XOR
+%%% USE SIMPLE_KEY_IMG TO CREATE XOR
+
+% subtract key_new1 values from og_img (may produce negative values)
+xor_img = og_img - key_new1;
+xor_img = mod(xor_img, 256);
 
 
-
-
-
+%=== DISPLAY XOR_IMG (for now)
+% scale image to fit the window (whether too big/small)
+imshow(xor_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
 
 end
+
+
--- Vigenere_Project/Image Encryption/key_to_img1.m
+++ Vigenere_Project/Image Encryption/key_to_img1.m
@@ -1,20 +1,21 @@
-function simple_key_img = key_to_img1(key, og_img)
-%%%CREATE AN IMAGE OF KEY, THAT IS THE SIZE OF OG_IMG
-% Done 'simply'; iterate word throughout each row
+function key_new1 = key_to_img1(key, og_img)
+%%%CREATE KEY_NEW1, AN IMAGE MADE FROM 'KEY' INPUT, THAT IS THE SIZE OF OG_IMG
+    % SUMMARY: characters of key (in an abbreviated ASCII code) are assigned to numeric values.
+    % these are then used to form an array of the same size as original [the
+    % values of numeric_k iterate through rows to do this]
 
-    %%% Inputs:
-    % key_new = key, repeated to match length of og_img
-    % og_img = original img input by user
-
-    %%%outputs: image created from key
-
+    %%% INPUTS:
+        % key_new1 = key (numeric values), repeated to match length of og_img
+        % og_img = original img input by user
+    
+    %%% OUTPUTS: image created from key
 
 %=== ACQUIRE CURRENT SIZE OF KEY
+colormap(gray);
 length_k = length(key);
 
 % numeric (double) version of key, accounting for desired ASCII start
 numeric_k = double(key) - 32;
-
 
 %=== MAKE KEY (NUMERIC) MATCH DIMENSIONS OF ORIGINAL IMAGE
 %if key is larger than image, confirm if user wants these inputs
@@ -22,33 +23,37 @@ if length_k > numel(og_img)
     disp(strcat('Are you sure you want', {' '}, key, ' as your key?'));
     % some if yes do x, if no do y
 
-    key_new = numeric_k;
+    key_new1 = numeric_k;
     return;
     %return; %IF WE TRIM DO HERE
     %%%%instead of trim: error message, confirm user wanted that order
 
-% if key = size of img, move on
+% IF KEY = SIZE OF OG_IMG, MOVE ON
 elseif length_k == numel(og_img)
-    key_new = numeric_k;
-    key_new = reshape(key_new, m, n);
+    key_new1 = numeric_k;
+    key_new1 = reshape(key_new1, m, n);
 
 
-% if size of og_img > length_k, repeat numeric_k to match
+% IF KEY < SIZE OF OG_IMG, REPEAT KEY TO MATCH SIZE
 else 
     % creates a vector, length og_img, counting up from 0
     vector_og = 0:numel(og_img) - 1;
     
-    % key_new = key, repeating until it's the size of img (call
+    % key_new1 = key, repeating until it's the size of img (call
     % remaining indices)
-    key_new = numeric_k(mod(og_img, length_k) + 1);
-    key_new = reshape(key_new, size(og_img)); %%need to confirm this works
+    key_new1 = numeric_k(mod(vector_og, length_k) + 1);
+    key_new1 = reshape(key_new1, size(og_img)); %%need to confirm this works
 
 end
 
 
-% MAKE KEY_NEW FUNCTIONAL AS IMAGE (values 0-255)
-key_new = mod(key_new, 256);
-sized = size(key_new)
+%=== MAKE KEY_NEW1 FUNCTIONAL AS IMAGE (values 0-255)
+key_new1 = mod(key_new1, 256);
 
+%=== DISPLAY KEY_NEW1 AS AN IMAGE? ??????????????
+colormap(gray);     %%% CHANGE IF WE DO 3 CHANNELS
+
+% Show image; will scale to fit user's window & use values 0-255
+imshow(key_new1, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
 
 end
