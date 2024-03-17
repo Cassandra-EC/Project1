a7cf4360b4672d8cb87eede8db99bb07b8ca1a36
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-16 21:49:44 -0400

    Working on encrypt_my_img
    Working for grayscale

--- Vigenere_Project/Image Encryption/encrypt_my_img.m
+++ Vigenere_Project/Image Encryption/encrypt_my_img.m
@@ -14,6 +14,12 @@ key_to_img2(og_img, key);
 
 encrypted_img = create_xor + key_to_img2;
 
+imagesc(shared_key));
+colormap(gray);
+title('Key as Image');
+colorbar;
+
+
 
 %=== ACQUIRE SIZE OF KEY & OG_IMG
 % (later will make image from key matching og)
@@ -24,22 +30,40 @@ length_k = length(key);
 numeric_k = double(key) - 32;
 
 
-%=== MAKE 
-%if key is larger than image, irrelevant; move on
+%=== Check length of key compared to image
 if length_k > numel(og_img)
-    disp(strcat('Are you sure you want', {' '}, key, ' as your key?'));
-    % some if yes do x, if no do y
+    user_input = input(['The length of the key is longer than the # of elements in your image!', ...
+        'Are you sure you want"', key, '" as your key? (yes/no): '], 's');
+    user_input = lower(user_input) % to avoid case-sensitive issues
+    
+    if strcmp(user_input, 'no') 
+        % if user made mistake, enter new key
+        key = input('Please enter your new key: ', 's')
+    elseif ~strcmp(user_input, 'yes');
+        disp('Try again. Please enter "yes" or "no".');
+        return;
+    end
+end
 
+% Numeric key, accounting for ASCII range
+numeric_k = double(key) - 32;
+
+% Make new key vector match the size of the original image
+if length_k < numel(og_img)
+    % Repeat the key until it matches og_img size
+    repeat_n = ceil(numel(og_img) / length_k);
+    key_new = repmat(numeric_k, 1, repeat_n);
+    key_new = key_new(1:numel(og_img));
+else
     key_new = numeric_k;
-    %return; %IF WE TRIM DO HERE
-    %%%%instead of trim: error message, confirm user wanted that order
+end
 
-% if key = size of img, move on
-elseif length_k == numel(og_img)
-    key_new = numeric_k;
+% Reshape new key to match image size
+key_
 
-% if length_p > length_k, repeat key until same length as plain
-else 
+
+
+
     % creates a vector, length og_img, counting up from 0
     vector_og = 0:numel(og_img) - 1;
     
