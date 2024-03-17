abcef124e97ac56a6a0a0d18bf74954ed7df9f82
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-17 16:46:28 -0400

    Spelling
    ....

--- Vigenere_Project/Image Encryption/encrypt_my_img.m
+++ Vigenere_Project/Image Encryption/encrypt_my_img.m
@@ -27,7 +27,7 @@ end
 
 % Create encrypted image
 encrypted_img = double(xor_img) + double(key_new2);
-encrypted_img = mod(encrypted_img, 256); %wraparound if eneded
+encrypted_img = mod(encrypted_img, 256); %wraparound if needed
 
 % Display original and encrypted images
 figure;
