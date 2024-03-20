b7ca944ac3b6dd703b7a2da1bfec83ac86527a01
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-20 18:38:02 -0400

    Probably ignore
    further attemps on decryption fixes, probably making it all worse....

--- Vigenere_Project/Image Encryption/decrypt_my_img.m
+++ Vigenere_Project/Image Encryption/decrypt_my_img.m
@@ -20,7 +20,6 @@ key_new1 = key_to_img1(encrypted_img, key);
 % key_new2
 key_new2 = key_to_img2(size(encrypted_img), key);
 key_new2 = uint8(key_new2);
-reciprocal_key_new2 = 1 ./ double(key_new2);
 
 %=== RETURN OG_IMG FROM XOR_IMG (in turn from encrypted img)
 % it is SO CLOSE, but there is definite variability
@@ -28,12 +27,16 @@ reciprocal_key_new2 = 1 ./ double(key_new2);
 %%% THIS IS SUPER FUCKED UP NOW. NEED TO FIX DECRYPTION IF WE OFFER IT
 % (& go through everything to debug. Make sure equations are all correct)
 %%% xor_img - key_new1 = og_img 
+not_zero_indices = key_new2 ~= 0; %when reciprocal key_new2 is not 0
+return_og_img = (return_xor_img);
+return_og_img(not_zero_indices) = return_og_img(not_zero_indices) .* key_new2(not_zero_indices);
 
-return_og_img = double(return_xor_img) .* (reciprocal_key_new2); %issue with 0 as divisor??
+
+%return_og_img = double(return_xor_img) .* (reciprocal_key_new2); %issue with 0 as divisor??
 return_og_img = mod(return_og_img, 256); % Wraparound if needed
 return_og_img = uint8(return_og_img);
 
-figure(1)
+figure(4)
 imshow(return_og_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
 title('OG Image (return from encrypted)');
 axis off;
--- Vigenere_Project/Image Encryption/decryption_attempt_sp.m
+++ Vigenere_Project/Image Encryption/decryption_attempt_sp.m
@@ -1,66 +1,42 @@
 %%%%%%%% messing around with a new script....
-
-%%% SPIRAL KEY IS CORRECT, at least... 
 function decrypt_img = decryption_attempt_sp(encrypted_img, key)
 
-% get key_new1
+% generate Key Image 1 (key_new1)
 key_new1 = key_to_img1(encrypted_img, key);
-key_new1 = double(key_new1);
 
-% get key_new2
-key_new2 = key_to_img2(encrypted_img, key);
+% generate Key Image 2 (key_new2)
+key_new2 = key_to_img2(size(encrypted_img), key);
 
-figure;
-    imshow(key_new2, 'DisplayRange', [0, 255], 'InitialMagnification', 'fit');
-    title('Key 2 (spiral)');
-    axis off;
+%key_new2 = uint8(key_new2);
 
-% reverse operation used to create xor_img
-%encrypted_img = double(xor_img) .* double(key_new2);
-return_xor_img = return_xor(encrypted_img, key);
-return_xor_img = double(return_xor_img);
+% handle division by 0 cases
+zero_replacement = 10^(-5);
+key_new2_nozeros = double(key_new2);
 
-encrypted_img = double(encrypted_img);
-decrypt_img = encrypted_img ./ double(key_new2);
-decrypt_img = double(decrypt_img);
+zeros_indices = key_new2_nozeros == 0; % indices where key_new2 = 0
+key_new2_nozeros(zeros_indices) = zero_replacement; %replace zeros with small number
 
-%xor_img = double(og_img) + double(key_new1); 
-decrypt_img = decrypt_img - return_xor_img;
+% Debug
+disp(size(zeros_indices)); % Should be the same as size(encrypted_img)
+disp(size(key_new2_nozeros)); % Should be the same as size(encrypted_img)
+
+% Decrypt XOR Image (division between encrypted and key_new2)
+%%% may be getting error due to division by 0-- there are 0's in my test
+%%% key_new
+xor_img = double(encrypted_img) ./ double(key_new2_nozeros);
+
+% Subtract key_new1 from XOR to get original image
+decrypt_img = xor_img - key_new1
+
+% Range for decrypt
 decrypt_img = mod(decrypt_img, 256);
+
 decrypt_img = uint8(decrypt_img);
 
-%xor_img = encrypted_img ./ double(key_new2);
-
-%xor_img_redchannel = xor_img(:,:,1);
-
-%figure;
-%   imshow(xor_img, 'DisplayRange', [0, 255], 'InitialMagnification', 'fit');
-%   title('XOR Image (Original + Key)');
-%   axis off;
-
-% get key_new1
-%key_new1 = key_to_img1(xor_img_redchannel, key);
-
-%figure;
-%   imshow(key_new1, 'DisplayRange', [0, 255], 'InitialMagnification', 'fit');
-%   title('Key 1 (from XOR Image)');
-%   axis off;
-
-% Get og_img (reverse operation for creating xor
-%xor_img_redchannel = cast(xor_img_redchannel, class(key_new1));
-%decrypt_img_redchannel = xor_img_redchannel - key_new1;
-
-%decrypt_img_redchannel = mod(decrypt_img_redchannel, 256);
-
-%decrypt_img_redchannel = uint8(decrypt_img_redchannel);
-
-% Combine decrypted red channel with original blue and green channels
-%decrypt_img = cat(3, decrypt_img_redchannel, encrypted_img(:,:,2:3));
-
-figure(4)
+figure(1)
 imshow(decrypt_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
 title('OG Image (return from encrypted)');
 axis off;
+
+
 end
-
-
