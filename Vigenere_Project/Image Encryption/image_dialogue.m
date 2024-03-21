b90400b189949f2d1d4d8f052387a47736c35ad9
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-20 23:29:49 -0400

    Input dialogue
    Working, need to make the loop work so it restarts if user inputs cancel or says no or something.

--- Vigenere_Project/Image Encryption/image_dialogue.m
+++ Vigenere_Project/Image Encryption/image_dialogue.m
@@ -1,9 +1,5 @@
 % created 3/18 CC
-
-
-function og_img = image_dialogue()
-
-
+% Edited 03/20 SP
 
 
 %%% DEALING WITH DIALOGUE BOXES & INPUTs
@@ -28,192 +24,105 @@ function og_img = image_dialogue()
 %Call this at start of encrypt_my_img
 
 % === INSTRUCTIONS TO USER FOR HOW TO USE FUNCTION
+function encrypted_img = image_dialogue()
 
+% Loop until selection or cancel
+while true
+    options = {'Upload image from file', 'Provide image URL', 'Create image array'};
+    % gives 3 options, if user hits return, defaults to upload from file
+    image_preference = questdlg('How would you like to upload your image?', 'Image Upload', options{1}, options{2}, options{3}, options{1});
 
-% === BOX TO ENTER KEY
+    if isempty(image_preference)
+        error('Please enter a preference for your image upload (either 1, 2, or 3)');
+    end
 
-
-% === INSTRUCTIONS FOR IMAGE REQUIREMENTS
-
-
-% === DIALOGUE TREE: 3 OPTIONS FOR IMAGE UPLOAD
-
-options = {'Upload image from file', 'Provide image URL', 'Create image array'};
-% gives 3 options, if user hits return, defaults to upload from file
-image_preference = questdlg('How would you like to upload your image?', 'Image Upload', options{1}, options{2}, options{3}, options{1});
-
-if isempty(image_preference)
-    error('Please enter a preference for your image upload (either 1, 2, or 3)');
+    % if user selects upload image from file
+    if strcmp(image_preference, options{1})
+        disp("Excellent. You've chosen to upload an image file from your device.")
+        [file_name, file_path] = uigetfile({'*.jpg; *.jpeg; *.png', 'Image Files'}, 'Select Image File');
+        % case for user cancelling input
+        if isequal(file_name, 0) || isequal(file_path, 0)
+            error('No image file selected.');
+        end
+    
+        % load and read image for encryption
+        img_path = fullfile(file_path, file_name);
+        og_img = imread(img_path);
+        break; % Exit loop
+    
+    % if user selects provide image URL
+    elseif strcmp(image_preference, options{2})
+        img_url = inputdlg('Enter the URL of your image:', 'Image URL', [1, 100]);
+        % case for no URL provided
+        if isempty(img_url)
+            error('No image URL entered.');
+        end
+    
+        % Extract the URL (in position {1} of img_url)
+        img_url = img_url{1};
+    
+        try
+            % Attempt to read image from URL
+            og_img = webread(img_url);
+    
+            imshow(og_img);
+            title('Downloaded Image');
+            axis off;
+            break; % Exit loop
+        catch
+            error('Failed to read your image from provided URL.');
+        end
+    
+    % if user selects array upload option
+    elseif strcmp(image_preference, options{3})
+         disp("A fellow coder, how nice! You've chosen to create an array to use as your image.");
+         disp("Please create your image array in the variable 'og_img'.");
+    
+         msgbox("Create your image array. Click OK when you're done!", 'Image Array Creation');
+        
+         % Wait for user to click
+         uiwait(msgbox("Create your image array. Click OK when you're done!", 'Image Array Creation'));
+    
+         if ~exist('og_img', 'var')
+             error('No image array created. Please make sure to define the variable ''og_img'' as your image!');
+         end
+         break; % Exit loop
+    
+    % If somehow user does something else...
+    else
+        error('Invalid option selected.');
+    end
 end
 
-% if user selects upload image from file
-if strcmp(image_preference, options{1})
-    disp("Excellent. You've chosen to upload an image file from your device.")
-    [file_name, file_path] = uigetfile({'*.jpg, *.jpeg, *.png', 'Image Files'}, 'Select Image File');
-    % case for user cancelling input
-    if isequal(file_name, 0) || isequal(file_path, 0)
-        error('No image file selected.');
-    end
-    
-    % load and read image for encryption
-    img_path = fullfile(file_path, file_name);
-    og_img = imread(img_path);
-
-% if user selects provide image URL
-elseif strcmp(image_preference, options{2})
-    img_url = inputdlg('Enter the URL of your image:', 'Image URL', [1, 100]);
-    % case for no URL provided
-    if isempty(img_url)
-        error('No image URL entered.');
-    end
-
-    % Extract the URL (in position {1} of img_url)
-    img_url = img_url{1};
-
-    try
-        % Attempt to read image from URL
-        og_img = webread(img_url);
-
-        imshow(og_img);
-        title('Downloaded Image');
-        axis off;
-    catch
-        error('Failed to read your image from provided URL.');
-    end
-
-%%% array upload option
-
-else
-    error('Invalid option selected.');
-end
-    
 % Display image for confirmation
 imshow(og_img);
 title('Your Image');
 axis off;
-
-confirm_img = questdlg('Is this the image you would like to encrypt?', 'Confirm Image', 'Yes', 'No', 'Yes');
-if isempty(confirm_img) || strcmp(confirm_img, 'No')
-    error('Image selection canceled.');
-end
-
-%=== CHECK IF IMG IS VIABLE; OTHERWISE MAKE TRY AGAIN
-
-
-
-
-
-    %=== CHECK IF USER WANTS THIS IMG; OTHERWISE MAKE TRY AGAIN
-    disp(og_img);
-    confirm_img_prompt = "Is this the image you would like to use? Y/N: ";
-    confirm_img = input(confirm_img_prompt, "s");
     
-    if contains(confirm_img, 'y', IgnoreCase=true)==Y
-        disp("Sounds good. Now let's encrypt your image.");
-        % encrypt_my_img called here??
-    else
-        %=== send back to either 1) file selection or 2) back to start?
+confirm_encrypt = questdlg('Is this the image you would like to encrypt?', 'Confirm Image and Encryption', 'Encrypt', 'Cancel', 'Encrypt');
 
-
+% Check user's response
+if strcmp(confirm_encrypt, 'Encrypt')
+    % Ask user for key input
+    key_cell = inputdlg('Enter an encryption key:', 'Key Input', [1, 50]);
+        
+    % Check for key
+    if isempty(key_cell)
+        error('No encryption key provided.');
     end
-
-
-elseif contains(image_preference, '2')==1 && contains(image_preference, '1')==0 && contains(image_preference, '3')==0 
-    disp("Very nice. You've chosen to provide a url to your desired img.")
-    %=== INSERT DIALOG BOX FOR URL
     
-    % og_img = ????
-
-
-    %=== CHECK IF IMG IS VIABLE; OTHERWISE MAKE TRY AGAIN
-    % 1) CONFIRM RIGHT TYPE OF FILE
-
-    % 2) CONFIRM OKAY SIZE
-
-
-
-    %=== CHECK IF USER WANTS THIS IMG; OTHERWISE MAKE TRY AGAIN
-    disp(og_img);
-    confirm_img_prompt = "Is this the image you would like to use? Y/N: ";
-    confirm_img = input(confirm_img_prompt, "s");
+    % Extract key (key cannot be used when cell array)
+    key = key_cell{1};
     
-    if contains(confirm_img, 'y', IgnoreCase=true)==Y
-        disp("Sounds good. Now let's encrypt your image.");
-        % encrypt_my_img called here??
-    else
-        %=== send back to either 1) file selection or 2) back to start?
-
-
-    end
-
-
-elseif contains(image_preference, '3') == true
-    disp("A fellow coder, how nice! You've chosen to create an array to use as your image.")
-    disp("We'll give you all the time you need to create your image. " + ...
-        "Set your matrix equal to 'og_img', and once done, just type 'DONE' in the command window!")
-
-    disp("");
-    disp("OG_IMG CREATION WINDOW");
-
-
-
-    %=== ALLOW TO CODE AND CREATE DESIRED OG_IMG
-
-
-    %=== ONCE 'DONE' IS TYPED, MOVE ON
-
-   
-    %=== CHECK IF IMG IS VIABLE; OTHERWISE MAKE TRY AGAIN
-    % 1) CONFIRM THAT IT'S NOT EMPTY
-
-    % 2) CONFIRM THAT IT'S THE RIGHT TYPE OF INPUT (I GUESS DOUBLE & UINT8)
-
-    % 3) CONFIRM OKAY SIZE
-
-
-    %=== CHECK IF USER WANTS THIS IMG; OTHERWISE MAKE TRY AGAIN
-    disp(og_img);
-    confirm_img_prompt = "Is this the image you would like to use? Y/N: ";
-    confirm_img = input(confirm_img_prompt, "s");
-    
-
-    if contains(confirm_img, 'y', IgnoreCase=true)==Y
-        disp("Sounds good. Now let's encrypt your image.");
-        % encrypt_my_img called here??
-
-    else
-        %=== send back to either 1) img creation or 2) back to start?
-
-
-    end
-
-
-
+    % Encrypt!
+    encrypted_img = encrypt_my_img(og_img, key);
+    disp('Encryption successful!');
+elseif strcmp(confirm_encrypt, 'Cancel')
+    disp('Encryption canceled.');
+    encrypted_img = [];
 else 
+    disp('Unexpected error. Encryption canceled.');
+    encrypted_img = [];
 end
 
-
-
-
-
-
-% choices:
-% 1) mandatory: keys
-% - checks that key is valid (if not try again)
-% 
-% 
-% 2) checkboxes? DECISION TREE [potentially disp, await response, pick 1/2/3]
-% if size is too big, make user try again
-% - A) upload file from computer
-        % SN's function
-% - B) input URL
-        % diff function, give url it does webread/imread
-% - C) create your own matrix
-        % type in DONE when complete? 
-% --> next
-% 
-% 
-% 3) next --> confirm both inputs look good (otherwise return to user)
-% 
-
-
+end
