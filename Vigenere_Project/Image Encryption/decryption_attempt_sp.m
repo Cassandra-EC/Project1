0132f1354ca5780f6a667d6c030e29a885485603
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-21 10:59:32 -0400

    It runs.. but does not work
    Title. I want to spend time making a dialogue tree that also allows for text encryption, so I'll work on that today.

--- Vigenere_Project/Image Encryption/decryption_attempt_sp.m
+++ Vigenere_Project/Image Encryption/decryption_attempt_sp.m
@@ -1,27 +1,19 @@
 %%%%%%%% messing around with a new script....
 function decrypt_img = decryption_attempt_sp(encrypted_img, key)
 
-% Get size of encrypted image
-[rows, cols, ~] = size(encrypted_img);
-disp('Size of encrypted_img:');
-    disp(size(encrypted_img));
-    disp('Data type of encrypted_img:');
-    disp(class(encrypted_img));
-
-
 % generate Key Image 1 (key_new1)
 key_new1 = key_to_img1(encrypted_img, key);
 
 % generate Key Image 2 (key_new2)
-key_new2 = key_to_img2([rows, cols], key);
+key_new2 = key_to_img2(encrypted_img, key);
 
 %key_new2 = uint8(key_new2);
 
 % handle division by 0 cases
-%zero_replacement = 1;
+zero_replacement = 10^(-5);
 
 key_new2_nozeros = key_new2;
-key_new2_nozeros(key_new2_nozeros == 0) = 1; %reassign zeros to 1?
+key_new2_nozeros(key_new2_nozeros == 0) = zero_replacement; %reassign zeros to 1?
 
 %zeros_indices = key_new2_nozeros == 0; % indices where key_new2 = 0
 %key_new2_nozeros(zeros_indices) = zero_replacement; %replace zeros with small number
@@ -36,7 +28,7 @@ key_new2_nozeros(key_new2_nozeros == 0) = 1; %reassign zeros to 1?
 decrypt_xor_img = double(encrypted_img) ./ double(key_new2_nozeros);
 
 % Subtract key_new1 from XOR to get original image
-decrypt_img = decrypt_xor_img - key_new1;
+decrypt_img = decrypt_xor_img - double(key_new1);
 
 % Range for decrypt
 decrypt_img = mod(decrypt_img, 256);
