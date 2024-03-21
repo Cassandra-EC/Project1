9040181ddbfbf5c7e6739317795dde3f2940e4d3
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-21 00:37:50 -0400

    Issue==
    This decryption is running into a big issue when it comes to key_to_img2. It seems that the key is being recognized as shorter than the image input (despite this not being the case), and is then being trimmed down. That means the arrays are not the same and further operations can't run. WIll see what I can do with this later

--- Vigenere_Project/Image Encryption/decryption_attempt_sp.m
+++ Vigenere_Project/Image Encryption/decryption_attempt_sp.m
@@ -1,12 +1,13 @@
 %%%%%%%% messing around with a new script....
 function decrypt_img = decryption_attempt_sp(encrypted_img, key)
+
+% Get size of encrypted image
+[rows, cols, ~] = size(encrypted_img);
 disp('Size of encrypted_img:');
     disp(size(encrypted_img));
     disp('Data type of encrypted_img:');
     disp(class(encrypted_img));
 
-% Get size of encrypted image
-[rows, cols, ~] = size(encrypted_img);
 
 % generate Key Image 1 (key_new1)
 key_new1 = key_to_img1(encrypted_img, key);
@@ -17,11 +18,13 @@ key_new2 = key_to_img2([rows, cols], key);
 %key_new2 = uint8(key_new2);
 
 % handle division by 0 cases
-% zero_replacement = 10^(-5);
-% key_new2_nozeros = double(key_new2);
+%zero_replacement = 1;
 
-% zeros_indices = key_new2_nozeros == 0; % indices where key_new2 = 0
-% key_new2_nozeros(zeros_indices) = zero_replacement; %replace zeros with small number
+key_new2_nozeros = key_new2;
+key_new2_nozeros(key_new2_nozeros == 0) = 1; %reassign zeros to 1?
+
+%zeros_indices = key_new2_nozeros == 0; % indices where key_new2 = 0
+%key_new2_nozeros(zeros_indices) = zero_replacement; %replace zeros with small number
 
 % Debug
 % disp(size(zeros_indices)); % Should be the same as size(encrypted_img)
@@ -30,10 +33,10 @@ key_new2 = key_to_img2([rows, cols], key);
 % Decrypt XOR Image (division between encrypted and key_new2)
 %%% may be getting error due to division by 0-- there are 0's in my test
 %%% key_new
-xor_img = double(encrypted_img) ./ double(key_new2);
+decrypt_xor_img = double(encrypted_img) ./ double(key_new2_nozeros);
 
 % Subtract key_new1 from XOR to get original image
-decrypt_img = xor_img - key_new1
+decrypt_img = decrypt_xor_img - key_new1;
 
 % Range for decrypt
 decrypt_img = mod(decrypt_img, 256);
