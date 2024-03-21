7516a7f533cef2726c114fc8b2f4b5b75bdfe1f7
sophianicogos <162854423+sophianicogos@users.noreply.github.com>
2024-03-21 19:44:48 -0400

    Ignore, decryption

--- Vigenere_Project/Image Encryption/decryption_attempt_sp.m
+++ Vigenere_Project/Image Encryption/decryption_attempt_sp.m
@@ -1,44 +1,39 @@
 %%%%%%%% messing around with a new script....
-function decrypt_img = decryption_attempt_sp(encrypted_img, key)
+function return_og_img = decryption_attempt_sp(encrypted_img, key)
+%%% Returns original image from return_xor_image and key. Final step in
+%%% decryption
 
-% generate Key Image 1 (key_new1)
+
+%=== LOAD XOR_IMG & KEY_NEW1
+% both use encrypted_img as a size reference rather than og_img. Key is
+% dependent on the size of image, rather than content
+
+% xor
+return_xor_img = return_xor(encrypted_img, key);
+
+%%% key_new1
 key_new1 = key_to_img1(encrypted_img, key);
 
-% generate Key Image 2 (key_new2)
-key_new2 = key_to_img2(encrypted_img, key);
 
-%key_new2 = uint8(key_new2);
 
-% handle division by 0 cases
-zero_replacement = 10^(-5);
+%=== RETURN OG_IMG FROM XOR_IMG (in turn from encrypted img)
+% it is SO CLOSE, but there is definite variability
 
-key_new2_nozeros = key_new2;
-key_new2_nozeros(key_new2_nozeros == 0) = zero_replacement; %reassign zeros to 1?
+%%% THIS IS SUPER FUCKED UP NOW. NEED TO FIX DECRYPTION IF WE OFFER IT
+% (& go through everything to debug. Make sure equations are all correct)
+%%% xor_img - key_new1 = og_img 
+return_og_img = double(return_xor_img) - double(key_new1);
+return_og_img = mod(return_og_img, 256); % Wraparound if needed
 
-%zeros_indices = key_new2_nozeros == 0; % indices where key_new2 = 0
-%key_new2_nozeros(zeros_indices) = zero_replacement; %replace zeros with small number
+%return_og_img = floor(mod(return_og_img, 256)); % Wraparound if needed
 
-% Debug
-% disp(size(zeros_indices)); % Should be the same as size(encrypted_img)
-% disp(size(key_new2_nozeros)); % Should be the same as size(encrypted_img)
-
-% Decrypt XOR Image (division between encrypted and key_new2)
-%%% may be getting error due to division by 0-- there are 0's in my test
-%%% key_new
-decrypt_xor_img = double(encrypted_img) ./ double(key_new2_nozeros);
-
-% Subtract key_new1 from XOR to get original image
-decrypt_img = decrypt_xor_img - double(key_new1);
-
-% Range for decrypt
-decrypt_img = mod(decrypt_img, 256);
-
-decrypt_img = uint8(decrypt_img);
-
-figure(1)
-imshow(decrypt_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
+figure(4)
+imshow(return_og_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
 title('OG Image (return from encrypted)');
 axis off;
+
+
+
 
 
 end
