20f9418aa4bf6439d7143c20b6e138dab60b32ac
Cassandra-EC <162471376+Cassandra-EC@users.noreply.github.com>
2024-03-22 16:54:29 -0400

    Updated encryption. key_new3 instead of xor
    This is updated to accommodate removal of the xor function. Should just use keys 1 and 2 now
    
    Needs to add extra figure at the end!!!
    
    But commenting & stuff is (I think/hope) pretty clear now

--- Vigenere_Project/Image Encryption/encrypt_my_img.m
+++ Vigenere_Project/Image Encryption/encrypt_my_img.m
@@ -6,88 +6,109 @@
 % edited 3/22 CC [NEW BRANCH CREATED, NEW IDEA]
 
 function encrypted_img = encrypt_my_img(og_img, key)
+% ENCRYPT_MY_IMG 'encrypts' a submitted image according to a submitted key
+% word/words in ASCII format. 
+% The key is converted into two matrices the same size as og_img (i.e. also
+% images!) that are used to manipulate the original image into an encrypted
+% version. The formula is as follows: og_img + (key_new1*key_new2) =
+% encrypted_img
 
-%%%% make alternate way to save encrypted_img (out of subplots) for
-%%%% decryption purposes. For when code works, of course
+% SUMMARY: encrypt a submitted image using a 'key' input (ASCII characters
+% accepted). Can also decrypt using the decrypt_my_img function
+
+    %%% INPUTS:
+        % og_img = an original input image
+        % key = some text (ASCII non-alphabetical characters also accepted)
+        % used to encrypt the og_img
+    
+    %%% OUTPUTS: 
+        % encrypted_img = a new image made using both og_img and the key
 
 
-%=== CALL OG_IMG BASED ON USER'S METHOD
-
-% %=== confirm class of variables
-% % ensure og_img is uint8
-% og_img = uint8(og_img);
-% class(og_img);
-% class(key_new1);
-% class(xor_img);
-% class(key_new2);
-
-%=== ENCRYPT THE XOR TO GET FINAL ENCRYPTED IMG
-% xor_img + key_new2 = encrypted_img 
-
-%%% NEED TO CALL KEY TO IMG 1
+%=== KEY PREPARATION ===%
+%%% CALL KEY_NEW1: img from 'key' numeric values repeating through rows
 key_new1 = uint8(key_to_img1(og_img, key));
 
-%%%NEED TO CALL CREATE XOR
-xor_img = uint8(create_xor(og_img, key_new1));
 
-% Make key matrix (Call key_to_img2)
+%%% CALL KEY_NEW2: img from 'key' numeric values repeating in a spiral (from center)
 key_new2 = uint8(key_to_img2(og_img, key));
 
 
-% [debug] Confirm that og_img and key_new2 are the same size
-if ~isequal(size(og_img), size(key_new2))
-    disp('size issue'); %%REMOVE LATER, for debugging reasons
-    size(og_img)
-    size(key_new2)
-end
+%%% SET KEY_NEW3: img from key_new1 x key_new2 (element-wise)
+key_new3 = key_new1.*key_new2;
+%key_new3 made to fit 0-256. not a necessity! just for display if desired
+mod(key_new3, 256);         
+disp(key_new3);
 
 
-% Create encrypted image
-% xor * key_new2 = encrypted img
-encrypted_img = double(xor_img) .* double(key_new2);
-%%%
-% encrypted_img = mod(encrypted_img, 256); %wraparound if needed
-%%%% TESTING NO MOD (maybe source of lost values)
-encrypted_img = min(max(encrypted_img, 0), 255); 
+%%% ENCRYPT IMAGE: new img from og_img + key_new3
+encrypted_img = double(og_img) .* double(key_new3);
+mod(encrypted_img, 256);
 
-%=== DISPLAY ORIGINAL & ENCRYPTED IMAGES
+
+
+%=== DISPLAY ORIGINAL & ENCRYPTED IMAGES ===%
+%CC:ERROR I think goal should be (if possible) to open 2 windows: 
+% window1: shows  user every stage of their img (og, key1, key2, key3, and
+% encrypted). This should have VERY CLEAR LABELS, including window titled
+% 'for your benefit' or smth.
+
+% window2: shows encrypted_img ALONE. NO TITLE. NOTHING ELSE. This is so
+% that they can download & send to friend. One HAS to be able to download &
+% upload this img. And then decrypt it and have og_img return. Good to test
+% decryption with the stored variable, but REAL USERS wouldn't have
+% encrypted_img stored!! Computer needs to be able to handle it. 
+    % The window can be titled 'download and send your encrypted img' or smth
+% like that. But in the BAR, not as a 'title' within the window. In a spot
+% where it won't be seen upon decryption
+
+
 % Restore all images to uint8
+% CC:ERROR  Try with/without this part. May not be necessary but keeping it
+% for now.
 og_img = uint8(og_img);
-xor_img = uint8(xor_img);
+key_new1 = uint8(key_new1);
 key_new2 = uint8(key_new2);
+key_new3 = uint8(key_new3);
 encrypted_img = uint8(encrypted_img);
 
-%%% SHOW ORIGINAL IMAGE
+% first figure: all the info
+% CC:ERROR [already put but repeating here]: we need another figure w only
+% the encrypted_img
 figure;
-subplot(1, 5, 1);
+
+%%% SHOW ORIGINAL IMAGE
+subplot(3, 3, 2);   % middle spot of 1st row (3x3 grid)
 imshow(og_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
-title('Original Image');
+title('ORIGINAL:');
 axis off;
 
 
-%%% SHOW XOR IMAGE
-subplot(1, 5, 2);
-imshow(xor_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
-title('XOR Image (Original + Key)');
-axis off;
-
-%%% SHOW SPIRAL KEY
-subplot(1, 5, 3);
-imshow(key_new2, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
-title('Key 2 (spiral)');
-axis off;
-
-%%% SHOW FINAL IMAGE
-subplot(1, 5, 4);
-imshow(encrypted_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
-title('Encrypted Image');
-axis off;
-
-%%% KEY NEW 1 for debugging
-subplot(1, 5, 5);
+%%% SHOW KEY1 (straight across)
+subplot(3, 3, 4);
 imshow(key_new1, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
-title('Key new1');
+title('KEY1');
 axis off;
+
+%%% SHOW KEY2 (spiral)
+subplot(3, 3, 5);
+imshow(key_new2, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
+title('KEY2');
+axis off;
+
+%%% SHOW KEY3 (key_new1 .* key_new2)
+subplot(3, 3, 6);
+imshow(key_new3, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
+title('KEY3');
+axis off;
+
+%%% SHOW ENCRYPTED, FINAL IMG
+subplot(3, 3, 8);     % middle spot of last row (3x3 grid)
+imshow(encrypted_img, 'DisplayRange', [0,255], 'InitialMagnification', 'fit');
+title('ENCRYPTED');
+axis off;
+
+% ADD ANOTHER FIGURE HERE !!!!
 
 
 end
